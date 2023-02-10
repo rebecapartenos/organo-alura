43ae3973646d6fc8f29b08e212989d76212a61bc
Rebeca <rehdomingos123@gmail.com>
2023-02-10 20:58:17 -0300

    add form element component

---
+++ src/components/FormElement/FormElement.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+import './FormElement.css'
+
+class FormElement extends Component{
+    state = {
+
+    }
+
+    render(){
+        return(
+            <div className="form-div">
+                <label className="form-label">{this.props.labelName}</label>
+                <input placeholder={this.props.placeholder} name = {this.props.name} type = {this.props.type} className = "form-input"/>
+            </div>
+        );
+    }
+
+}
+export default FormElement;
