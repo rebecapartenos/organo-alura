32de267b2aec5a3cd7fba6a5057dc9604d0ecb6e
Rebeca <rehdomingos123@gmail.com>
2023-03-18 15:18:08 -0300

    add on click function

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -8,7 +8,7 @@ export default class Button extends Component {
     render() {
         return (
             <div>
-                <button id={this.props.id} className='button'>{this.props.label}</button>
+                <button id={this.props.id} className='button' onClick={this.props.callback}>{this.props.label}</button>
             </div>
         )
     }
